c4242617b3863c52dbc9732f8b6f4d48135040d4
AndreasAtakan <andreas@napkingis.no>
2020-08-17 12:02:55 +0200

    Final touches

--- napkin/main.php
+++ napkin/main.php
@@ -244,6 +244,199 @@
     $("span#ccYear").html(new Date().getFullYear());
   </script>
 
+	<script type="text/javascript">
+		function calcWidth(width) {
+			let W = window.innerWidth;
+
+			if(W > 1199)
+				return (1 - 1/6)*width;
+			else
+			if(W > 991)
+				return (1 - 1/6)*width;
+			else
+			if(W > 767)
+				return (1 - 3/12)*width;
+			else
+			if(W > 575)
+				return width;
+
+			return width;
+		}
+
+		function calcHeight(height) {
+			let W = window.innerWidth;
+
+			if(W > 1199)
+				return height - 54;
+			else
+			if(W > 991)
+				return height - 54;
+			else
+			if(W > 767)
+				return height - 54;
+			else
+			if(W > 575)
+				return height - 129;
+
+			return height - 129;
+		}
+
+
+
+		/* MapBox token */
+	  const MAPBOX_TOKEN = "pk.eyJ1IjoiYW5kcmVhc2F0YWthbiIsImEiOiJjazlndzM1cmUwMnl5M21tZjQ3dXpzeHJnIn0.oE5zp040ZzJj5QgCDznweg";
+
+	  /*
+	    Potential parks:
+	      Pidhorodnye Solar Park:   48.606888, 35.150520
+	      Starokozache Solar Park:  46.466667, 30.733333
+	      Perovo Solar Park:        44.900000, 33.933333
+	      Okhotnykovo Solar Park:   45.238889, 33.592778
+	  */
+
+	  /** STORE **/
+	  const reducers = (function createReducers(redux, keplerGl) {
+	    return redux.combineReducers({
+	      keplerGl: keplerGl.keplerGlReducer.initialState({
+	        mapState: { // norge – lat:60 lng:9 zoom:5
+	          latitude: 48.606888,
+	          longitude: 35.150520,
+	          zoom: 15
+	        },
+	        mapStyle: {
+	          styleType: "satellite",
+	          threeDBuildingColor: [ 192, 192, 192 ]
+	        },
+	        uiState: {
+	          currentModal: null,
+	          readOnly: true
+	        }
+	      })
+	    });
+	  }(Redux, KeplerGl));
+
+	  const middleWares = (function createMiddlewares(keplerGl) {
+	    return keplerGl.enhanceReduxMiddleware([
+	      // Add other middlewares here
+	    ]);
+	  }(KeplerGl));
+
+	  const enhancers = (function craeteEnhancers(redux, middles) {
+	    return redux.applyMiddleware(...middles);
+	  }(Redux, middleWares));
+
+	  const store = (function createStore(redux, enhancers) {
+	    const initialState = {};
+
+	    return redux.createStore(
+	      reducers,
+	      initialState,
+	      redux.compose(enhancers)
+	    );
+	  }(Redux, enhancers));
+	  /** END STORE **/
+
+	  /** COMPONENTS **/
+	  const KeplerElement = (function(react, keplerGl, mapboxToken) {
+	    return function(props) {
+	      let rootElm = react.useRef(null);
+
+	      let _useState = react.useState({
+	        width: calcWidth(window.innerWidth),
+	        height: calcHeight(window.innerHeight)
+	      });
+
+	      let windowDimension = _useState[0],
+	          setDimension = _useState[1];
+
+	      react.useEffect(function sideEffect() {
+	        function handleResize() {
+	          setDimension({
+	            width: calcWidth(window.innerWidth),
+	            height: calcHeight(window.innerHeight)
+	          });
+	        };
+	        window.addEventListener("resize", handleResize);
+	        return function() { window.removeEventListener("resize", handleResize); };
+	      }, []);
+
+	      return react.createElement(
+	        "div",
+	        { style: { /*position: "absolute", left: 0,*/ width: "100%", height: "100%" } },
+	        react.createElement(keplerGl.KeplerGl, {
+	          mapboxApiAccessToken: mapboxToken,
+	          id: "map",
+	          width:  windowDimension.width,
+	          height: windowDimension.height,
+	          appName: "",
+	          appWebsite: "https://napkingis.no/"
+	        })
+	      );
+	    };
+	  }(React, KeplerGl, MAPBOX_TOKEN));
+
+	  const app = (function createReactReduxProvider(react, reactRedux, KeplerElement) {
+	    return react.createElement(
+	      reactRedux.Provider,
+	      { store },
+	      react.createElement(KeplerElement, null)
+	    )
+	  }(React, ReactRedux, KeplerElement));
+	  /** END COMPONENTS **/
+
+	  /** Render **/
+	  (function render(react, reactDOM, app) {
+	    reactDOM.render(app, document.getElementById("app"));
+	  }(React, ReactDOM, app));
+
+
+	  /**
+	   * Customize map.
+	   * Interact with map store to customize data and behavior
+	   */
+	  (function customize(keplerGl, store) {
+	    //store.dispatch(keplerGl.toggleSplitMap());
+	    // let exportMapData = function() { return KeplerGl.KeplerGlSchema.save(store.getState().keplerGl.map); };
+
+			<?php
+				$eid = null;
+
+				if(isset($_GET['eid'])) $eid = $_GET['eid'];
+				else $eid = "d1c7b8ff-3a11-4834-86ee-81e21cf59ff5";
+
+				$stmt = $pdo->prepare("SELECT data->'config' AS config, data->'datasets' AS datasets FROM \"Entity\" WHERE entityid = ?");
+				$stmt->execute([$eid]);
+				$res = $stmt->fetch();
+
+				echo "
+					const datasets = ".$res['datasets'].";
+					const config = ".$res['config'].";
+
+					const loadedData = keplerGl.KeplerGlSchema.load(
+						datasets,
+						config
+					);
+
+					store.dispatch(keplerGl.addDataToMap({
+						datasets: loadedData.datasets,
+						config: loadedData.config,
+						options: {
+							centerMap: false
+						}
+					}));
+				";
+			?>
+
+	    /*store.dispatch(keplerGl.addDataToMap({
+	      datasets: loadedData.datasets,
+	      config: loadedData.config,
+	      options: {
+	        centerMap: false
+	      }
+	    }));*/
+	  }(KeplerGl, store));
+  </script>
+
 	<script src="js/index.js"></script>
 
 	<script src="js/main.php.js"></script>
