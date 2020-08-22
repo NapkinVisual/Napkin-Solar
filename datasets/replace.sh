f2e5ac4f3c36bd80aadefb5a910a5490c2e2bae4
AndreasAtakan <andreas@napkingis.no>
2020-08-22 22:25:39 +0200

    sync

--- datasets/replace.sh
+++ datasets/replace.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sed -e '/\/\*\[__REPLACE__\]\*\// {' -e 'r DATA.txt' -e 'd' -e '}' -i pidhorodnye_historical.html
+sed -e '/\/\*\[__REPLACE__\]\*\// {' -e 'r DATA.txt' -e 'd' -e '}' -i raw_pidhorodnye_historical.html
